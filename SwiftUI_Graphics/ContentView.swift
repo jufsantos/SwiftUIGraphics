2005289c7a8b4f5597dbc3e92322b21b32fd55b0
lffattori <luhf.fattori@icloud.com>
2020-06-10 16:58:14 -0300

    Initial Commit

---
+++ SwiftUI_Graphics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Graphics
+//
+//  Created by Luiza Fattori on 10/06/20.
+//  Copyright © 2020 Luiza Fattori. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
