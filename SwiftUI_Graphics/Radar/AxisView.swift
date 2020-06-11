bc7263215dc48a1406af9300e7ab82fb554e08c5
LeoAOliveira <leonardo.aoliveira@icloud.com>
2020-06-11 16:49:08 -0300

    Axis degree iteration

--- SwiftUI_Graphics/Radar/AxisView.swift
+++ SwiftUI_Graphics/Radar/AxisView.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct AxisComponent {
+struct AxisComponent: Hashable {
     
     var subtitle: String
     var scale: ClosedRange<Double>
@@ -27,15 +27,22 @@ struct AxisView: View {
     }
     
     var body: some View {
+        
         GeometryReader { geo in
-            LineView { () -> [CGPoint] in
-                let center = CGPoint(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
+            
+            ForEach(0..<self.components.count) { (index) in
                 
-                let nextX = Double(center.x) + 200 * cos(self.degreeInterval)
-                let nextY = Double(center.y) + 200 * sin(self.degreeInterval)
-                let nextPoint = CGPoint(x: nextX, y: nextY)
-                
-                return [center, nextPoint]
+                LineView { () -> [CGPoint] in
+                    let center = CGPoint(x: geo.frame(in: .global).midX, y: geo.frame(in: .global).midY)
+                    
+                    let interval = self.degreeInterval * Double(index + 1)
+                    
+                    let nextX = Double(center.x) + 150 * cos(interval)
+                    let nextY = Double(center.y) + 150 * sin(interval)
+                    let nextPoint = CGPoint(x: nextX, y: nextY)
+                    
+                    return [center, nextPoint]
+                }
             }
         }
     }
