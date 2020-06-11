30c9d2136f9987f1513f767af111300d8aeeda23
Galdineris <galdineris@gmail.com>
2020-06-11 14:15:04 -0300

    [PBXPROJ] Create ScatterPlotView.swift

---
+++ SwiftUI_Graphics/Scatterplot/ScatterPlotView.swift
@@ -0,0 +1,22 @@
+//
+//  ScatterPlotView.swift
+//  SwiftUI_Graphics
+//
+//  Created by Rafael Galdino on 11/06/20.
+//  Copyright © 2020 Luiza Fattori. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScatterPlotView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ScatterPlotView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScatterPlotView()
+    }
+}
